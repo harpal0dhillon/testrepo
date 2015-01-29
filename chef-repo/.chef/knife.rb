a88c16816cfd0f3d6486b4e80ccfbee97d3a5da4
harpal.s.dhillon <harpal.s.dhillon@accenture.com>
2015-01-29 06:56:48 -0800

    New change added

--- chef-repo/.chef/knife.rb
+++ chef-repo/.chef/knife.rb
@@ -4,9 +4,9 @@ current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
 node_name                "harpal"
-client_key               "#{current_dir}/harpal.pem"
+client_key               "/home/harpal/workspace/chef-repo/.chef/harpal.pem"
 validation_client_name   "cand-validator"
-validation_key           "#{current_dir}/cand-validator.pem"
+validation_key           "/home/harpal/workspace/chef-repo/.chef/cand-validator.pem"
 chef_server_url          "https://api.opscode.com/organizations/cand"
 cache_type               'BasicFile'
 cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
